d763a31e90b97ec1df82861634b7665b92968669
awallclock <42232299+awallclock@users.noreply.github.com>
2022-03-02 14:20:27 -0500

    initial commit

---
+++ InstigatorQuotes.lua
@@ -0,0 +1,105 @@
+local addonName, quotes = ...
+
+quoteChannel = "SAY"
+
+
+
+function tableLength(T)
+	local count = 0
+	for _ in pairs(T) do count = count + 1 end
+	return count
+end
+
+
+
+function quoteOut()
+local tableLen = tableLength(Quotes)
+local randomNumber = math.random(tableLen)
+local quote = Quotes(randomNumber)
+SendChatMessage(quote, quoteChannel)
+
+end
+
+
+commands = {
+	["yell"] = function(...)
+		quoteChannel = "YELL"
+		quoteOut()
+		end,
+	["y"] = function(...)
+		quoteChannel = "YELL"
+		quoteOut()
+		end,
+	["ra"] = function(...)
+		quoteChannel = "RAID"
+		quoteOut()
+		end,
+	["raid"] = function(...)
+		quoteChannel = "RAID"
+		quoteOut()
+		end,
+	["raidwarning"] = function(...)
+		quoteChannel = "RAIDWARNING"
+		quoteOut()
+		end,
+	["rw"] = function(...)
+		quoteChannel = "RAIDWARNING"
+		quoteOut()
+		end,
+	["party"] = function(...)
+		quoteChannel = "PARTY"
+		quoteOut()
+		end,
+	["p"] = function(...)
+		quoteChannel = "PARTY"
+		quoteOut()
+		end,
+	["say"] = function(...)
+		quoteChannel = "SAY"
+		quoteOut()
+		end,
+	["s"] = function(...)
+		quoteChannel = "SAY"
+		quoteOut()
+		end,
+}
+local function quoteCommands(str)
+
+	if (#str == 0) then
+		quoteOut()
+		end
+
+		local args = {};
+	for _, arg in ipairs({ string.split(' ', str) }) do
+		if (#arg > 0) then
+			table.insert(args, arg);
+		end
+	end
+	
+	local path = commands; -- required for updating found table.
+	
+	for id, arg in ipairs(args) do
+		if (#arg > 0) then -- if string length is greater than 0.
+			arg = arg:lower();			
+			if (path[arg]) then
+				if (type(path[arg]) == "function") then				
+					-- all remaining args passed to our function!
+					path[arg](select(id + 1, unpack(args))); 
+					return;					
+				elseif (type(path[arg]) == "table") then				
+					path = path[arg]; -- another sub-table found!
+				end
+			else
+				-- does not exist!
+				
+				return;
+			end
+		end
+	end
+end
+
+
+SLASH_INSTIGATORQUOTES1, SLASH_INSTIGATORQUOTES2 = '/gator', '/gatr', '/GATOR', '/gatr'
+SlashCmdList.INSTIGATORQUOTES = quoteCommands
+
+
---
+++ Quotes.lua
@@ -0,0 +1,6 @@
+local _, quotes = ...																									
+
+quotes = {
+"Who is munyanyo?",
+"Look at your hands!",
+}
