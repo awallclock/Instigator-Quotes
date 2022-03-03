ea3fb7c493d147631604323d52b69584948c9acb
awallclock <tim.witkov@gmail.com>
2022-03-02 20:52:23 -0500

    Update Quotes.lua

--- Quotes.lua
+++ Quotes.lua
@@ -2,5 +2,13 @@ local _, gatrQuotes = ...
 
 gatrQuotes.quote = {
 'Who is munyanyo?',
-'Look at your hands!',
+'Look at your hands!'
+'Cancelled',
+'Bing Bong!',
+'My dentist just called...'
+'Energy. Power. Cum. My people are addicted to it...'
+'Stay in your lane, Tethal'
+'It\'s going to look like I\'m hearthing'
+'BITE BITE GATOR BITE'
+
 }
