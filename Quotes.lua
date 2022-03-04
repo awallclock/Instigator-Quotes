cb2f5d3b7149d0458faf965bfccc59235ac1374f
awallclock <tim.witkov@gmail.com>
2022-03-03 20:02:19 -0500

    Update Quotes.lua

--- Quotes.lua
+++ Quotes.lua
@@ -5,18 +5,19 @@ gatrQuotes.quote = {
 'Look at your hands!'
 'Cancelled',
 'Bing Bong!',
-'My dentist just called...'
-'Energy. Power. Cum. My people are addicted to it...'
-'Stay in your lane, Tethal'
-'It\'s going to look like I\'m hearthing'
-'BITE BITE GATOR BITE'
-'CYA L8R IM A G8R'
-'GATOR ATTACK!'
-'BITE BITE!'
-'GATORS! I CHOOSE YOU!'
-'JOOOOE BIDEN'
-'Did you know Kentucky is the only state that starts with the letter K?'
-'CHOMPCHOMP'
-'GATRS BITCHES BEST BE WEARING JIMMIES'
+'My dentist just called...',
+'Energy. Power. Cum. My people are addicted to it...',
+'Stay in your lane, Tethal',
+'It\'s going to look like I\'m hearthing',
+'BITE BITE GATOR BITE',
+'CYA L8R IM A G8R',
+'GATOR ATTACK!',
+'BITE BITE!',
+'GATORS! I CHOOSE YOU!',
+'JOOOOE BIDEN',
+'Did you know Kentucky is the only state that starts with the letter K?',
+'CHOMPCHOMP',
+'GATRS BITCHES BEST BE WEARING JIMMIES',
+'Hello?',
 
 }
